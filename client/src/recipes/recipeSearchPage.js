e2028a7483270182ed61044a28aee8c1e45eb53c
davin <dizqar@gmail.com>
2021-04-17 00:53:09 +0700

    button click on enter trigger added

--- client/src/recipes/recipeSearchPage.js
+++ client/src/recipes/recipeSearchPage.js
@@ -5,34 +5,42 @@ import { RecipeSearchResultsList } from './RecipeSearchResultsList';
 import { useRecipeSearchResults } from './useRecipeSearchResults';
 
 export const RecipeSearchPage = () => {
-    const [searchString, setSearchString] = useState('');
-    const [searchInputValue, setSearchInputValue] = useState('');
-    const { ingredients } = useIngredients();
-    const { searchResults } = useRecipeSearchResults(searchString);
+  const [searchString, setSearchString] = useState('');
+  const [searchInputValue, setSearchInputValue] = useState('');
+  const { ingredients } = useIngredients();
+  const { searchResults } = useRecipeSearchResults(searchString);
 
-    const onSearchClicked = () => {
-        setSearchString(searchInputValue);
+  const onSearchClicked = () => {
+    setSearchString(searchInputValue);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onSearchClicked();
     }
+  };
 
-    return (
-        <div className="page">
-            <BackButton />
-            <div className="centered-container">
-                <h1>Add Meal to Plan</h1>
-                <input
-                    type="text"
-                    className="full-width space-before space-after"
-                    placeholder="Enter keyword here"
-                    value={searchInputValue}
-                    onChange={e => setSearchInputValue(e.target.value)} />
-                <button
-                    className="full-width space-after"
-                    onClick={onSearchClicked}
-                >Search</button>
-                <RecipeSearchResultsList
-                    recipes={searchResults}
-                    ingredients={ingredients} />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="page">
+      <BackButton />
+      <div className="centered-container">
+        <h1>Add Meal to Plan</h1>
+        <input
+          type="text"
+          className="full-width space-before space-after"
+          placeholder="Enter keyword here"
+          value={searchInputValue}
+          onChange={(e) => setSearchInputValue(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <button className="full-width space-after" onClick={onSearchClicked}>
+          Search
+        </button>
+        <RecipeSearchResultsList
+          recipes={searchResults}
+          ingredients={ingredients}
+        />
+      </div>
+    </div>
+  );
+};
